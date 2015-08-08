f5615f0361e8ec1a5d865851c47daef8d27a2430
Shane <shane8641@live.com>
2015-08-08 14:37:08 -0400

    Added Human class and tested implementation.

--- src/BattleBots.java
+++ src/BattleBots.java
@@ -6,10 +6,17 @@ public class BattleBots {
         System.out.println(o.getDamage());
         System.out.println(o.getHealth());
         System.out.println(o.getName());
+        System.out.println();
         // Testing the implementation of an Elf object
         Elf e = new Elf();
         System.out.println(e.getDamage());
         System.out.println(e.getHealth());
         System.out.println(e.getName());
+        System.out.println();
+        // Testing the implementation of an Elf object
+        Human h = new Human();
+        System.out.println(h.getDamage());
+        System.out.println(h.getHealth());
+        System.out.println(h.getName());
     }
 }
