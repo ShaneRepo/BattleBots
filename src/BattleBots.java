2f1b5edef60b4bb07ddcef82d0508c48421bed1c
Shane <shane8641@live.com>
2015-08-08 10:07:48 -0400

    Added Player base class

--- src/BattleBots.java
+++ src/BattleBots.java
@@ -1,6 +1,6 @@
 
 public class BattleBots {
     public static void main(String[] args){
-        
+
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,37 @@
+
+public class Player {
+    private int health;
+    private String name;
+    private int damage;
+
+    public Player(int health, String name, int damage) {
+        this.health = health;
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+}
