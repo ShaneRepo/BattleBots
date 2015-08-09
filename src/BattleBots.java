f28259036459268bba3e0b9c38f8c3e9a2f2ca37
Shane <shane8641@live.com>
2015-08-09 11:29:31 -0400

    Added the battle method and ran tests.

--- src/BattleBots.java
+++ src/BattleBots.java
@@ -1,22 +1,16 @@
-// Main class of the program
+
 public class BattleBots {
-    public static void main(String[] args){
-        // Testing the implementation of an Orc object
+    public static void main(String[] args) throws InterruptedException {
+
         Orc o = new Orc();
-        System.out.println(o.getDamage());
-        System.out.println(o.getHealth());
-        System.out.println(o.getName());
-        System.out.println();
-        // Testing the implementation of an Elf object
         Elf e = new Elf();
-        System.out.println(e.getDamage());
-        System.out.println(e.getHealth());
-        System.out.println(e.getName());
-        System.out.println();
-        // Testing the implementation of an Elf object
-        Human h = new Human();
-        System.out.println(h.getDamage());
-        System.out.println(h.getHealth());
-        System.out.println(h.getName());
+        Player.battle(o, e);
+
+
     }
+
+
 }
+
+
+
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 // Base class others will inherit from
 public class Player {
     private int health;
@@ -34,4 +36,50 @@ public class Player {
         this.damage = damage;
     }
 
+    public static void battle(Player p1, Player p2) throws InterruptedException {
+            int dam1 = 0;
+            int hp1 = 0;
+            int dam2 = 0;
+            int hp2 = 0;
+            boolean isDead = false;
+            do
+            {
+                dam1 = p1.getDamage();
+                System.out.println(p1.getName() + " Strikes for: " + p1.getDamage());
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                hp2 = (p2.getHealth() - dam1);
+
+                if (hp2 < 1)
+                {
+                    System.out.println(p1.getName() + " Is victorious! ");
+                    isDead = true;
+                    System.exit(0);
+                }
+                p2.setHealth(hp2);
+                dam2 = p2.getDamage();
+                System.out.println(p2.getName() + " Strikes for: " + p2.getDamage());
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                hp1 = (p1.getHealth() - dam2);
+
+                if (hp1 < 1)
+                {
+                    System.out.println(p2.getName() + " Is victorious! ");
+                    isDead = true;
+                    System.exit(0);
+                }
+                p1.setHealth(hp1);
+                System.out.println(p1.getName() + " HP " + p1.getHealth() + " --- " + p2.getName() + " HP " + p2.getHealth());
+            }while(!isDead);
+
+        }
 }
+
+
