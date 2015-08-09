db7b38f8d19e9b4f98b12dd24276854dff0d6c48
Shane <shane8641@live.com>
2015-08-09 13:37:31 -0400

    Updated class selection to start the game if validated.

--- src/BattleBots.java
+++ src/BattleBots.java
@@ -6,7 +6,7 @@ public class BattleBots {
         /*Orc o = new Orc();
         Elf e = new Elf();
         Player.battle(o, e);*/
-        int choice;
+        int choice, choice2;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Battle Bots!");
         do {
@@ -15,17 +15,27 @@ public class BattleBots {
             switch (choice) {
                 case 1:
                     System.out.println("You choose Human!");
+                    Human h = new Human();
+                    Orc o = new Orc();
+                    Player.battle(h,o);
                     break;
                 case 2:
                     System.out.println("You choose Orc!");
+                    o = new Orc();
+                    Elf e = new Elf();
+                    Player.battle(o,e);
                     break;
                 case 3:
                     System.out.println("You choose Elf!");
+                    e = new Elf();
+                    h = new Human();
+                    Player.battle(e,h);
                     break;
                 default:
                     System.out.println("Error! Please choose 1, 2, or a 3.");
             }
         }while(choice < 1 || choice > 3);
+
 
 
     }
